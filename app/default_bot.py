76b7e2d9d753ef58268aaf8295f414ddbcf5780a
Niko Junnila <niko.junnila@goodlife.technology>
2023-11-28 14:55:28 +0200

    Add ability to change OpenAI backend with OPENAI_API_BASE environment variable. Add CORS.

--- app/default_bot.py
+++ app/default_bot.py
@@ -1,7 +1,10 @@
+import os
 from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableLambda
+from langchain.chat_models import ChatOpenAI
 from models import AireChatInput
 
-
+model = ChatOpenAI(model="gpt-3.5-turbo", base_url=os.getenv("OPENAI_API_BASE"))
 system_prompt_text = """
 Act as a medical advisor.
 Your task is to find out what is bothering your patient and provide suggestions.
@@ -19,10 +22,12 @@ For emergencies, people should call 112 to get immediate help.
 It is important that you tell your patient that you are a bot.
 """
 
-def runnable(input: AireChatInput):
+def process(input: AireChatInput):
     history = map(lambda msg: (msg.name, msg.content), input.chat)
     prompt = ChatPromptTemplate.from_messages([
         ("system", system_prompt_text),
         *history
     ])
     return prompt
+
+DefaultBot = RunnableLambda(process) | model
--- app/server.py
+++ app/server.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
-from langchain.chat_models import ChatOpenAI
-from langchain.schema.runnable import RunnableLambda
 from models import AireChatbot
-import default_bot
+from default_bot import DefaultBot
 
 app = FastAPI(
     title="AIRe AI",
     version="0.1",
     description="AIRe AI Service Module"
 )
-model = ChatOpenAI(model="gpt-3.5-turbo")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def redirect_root_to_docs():
@@ -25,7 +31,7 @@ async def get_bots() -> list[AireChatbot]:
 
 add_routes(
     app, 
-    RunnableLambda(default_bot.runnable) | model, 
+    DefaultBot, 
     path="/api/bot/default")
 
 if __name__ == "__main__":
