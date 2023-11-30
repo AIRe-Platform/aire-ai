0e9aab4924b987b8c04fec73108d536a6c542818
Niko Junnila <niko.junnila@goodlife.technology>
2023-11-30 13:35:50 +0200

    Adjust prompts

--- app/aire/chains/user_context.py
+++ app/aire/chains/user_context.py
@@ -1,9 +1,7 @@
-import json;
-from langchain.prompts import HumanMessagePromptTemplate;
+import json
+from langchain.schema.messages import SystemMessage, HumanMessage
 from langchain.chat_models.base import BaseChatModel
 from ..models.user import AireUser
-
-template = HumanMessagePromptTemplate.from_template("Summarize user info from the following JSON: {json_str}")
 
 def summarize_user_context(user: AireUser, model: BaseChatModel):
     data = json.dumps(user.dict(include={
@@ -15,10 +13,12 @@ def summarize_user_context(user: AireUser, model: BaseChatModel):
         "country", 
         "bio"
     }))
-    prompt = template.format(json_str=data)
-    print(f"Prompt: {prompt}")
+    print(f"User data: {data}")
 
-    summary = model.invoke([prompt])
+    summary = model.invoke([
+        SystemMessage(content="Summarize user info from the given JSON object."),
+        HumanMessage(content=data)
+    ])
     print(f"Summary: {summary}")
 
     return summary.content
