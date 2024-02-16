f9c1981270409585d41c5e5ff0eb79533f1165af
Niko Junnila <niko.junnila@goodlife.technology>
2024-02-16 09:30:36 +0200

    Better keyword parsing

--- app/aire/chains/cbr_tagging.py
+++ app/aire/chains/cbr_tagging.py
@@ -1,7 +1,6 @@
 from langchain.chains import create_tagging_chain_pydantic
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain.schema.runnable import RunnableLambda
-from langchain.output_parsers import CommaSeparatedListOutputParser
 from ..models.chat import AireChatContext
 from ..models.cbr import CBR_KEYWORDS
 from ..llm import ChatModel
@@ -9,7 +8,7 @@ from ..llm import ChatModel
 class CbrTags(BaseModel):
     keywords: str = Field(..., 
                           enum=CBR_KEYWORDS,
-                          description="Describes what CBR topics could relate to the discussion, separated by commas and spaces")
+                          description="Describes what CBR topics could relate to the discussion, separated by commas.")
 
 def __cbr_tagging_chain(ctx: AireChatContext):
     llm = ChatModel(temperature=0.0)
@@ -17,7 +16,7 @@ def __cbr_tagging_chain(ctx: AireChatContext):
     messages = ctx.input.to_chat_messages()
     output = chain.invoke(messages)
     model: CbrTags = output['text']
-    parser = CommaSeparatedListOutputParser()
-    return parser.parse(model.keywords)
+    keywords = map(lambda x: x.strip() ,model.keywords.split(","))
+    return list(keywords)
 
 CbrTaggingChain = RunnableLambda(__cbr_tagging_chain)
