dd21de145285450a1d85a13cbdd77136a7c3e6d7
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-08-19 11:06:20 +0300

    Adapting AI module to MPL 2.0 License

--- app/bot/vector_stores.py
+++ app/bot/vector_stores.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
 from langchain_core.documents import Document
 from langchain_community.vectorstores.pgvector import PGVector
 from langchain_community.document_loaders.pdf import PyPDFLoader
--- app/server.py
+++ app/server.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
